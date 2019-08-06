2097a664c8bfb90d69e71d5c975131dc8868ec84
Martin Jurča <martin.jurca@firma.seznam.cz>
2019-08-06 12:17:47 +0200

    added more type checks

--- test-types.ts
+++ test-types.ts
@@ -6,3 +6,6 @@ const observeTypeCheck1: (target: Element, callback: EntryCallback) => void = ob
 const observeTypeCheck2: (target: Element, options: IntersectionObserverInit, callback: EntryCallback) => void = observe
 const unobserve = observe(document.body, (entry: IntersectionObserverEntry) => void 0)
 const unobserveTypeCheck: () => void = unobserve
+
+const observe2 = insularObserverFactory(IntersectionObserver)
+observe2(document.body, {rootMargin: '15px'}, () => void 0)
