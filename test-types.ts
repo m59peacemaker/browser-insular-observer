443dec719285f8eadecc142f76d9243da6d2536a
Martin Jurča <martin.jurca@firma.seznam.cz>
2019-08-06 12:16:24 +0200

    added a type check to the test command

---
+++ test-types.ts
@@ -0,0 +1,8 @@
+import insularObserverFactory = require('.')
+
+const observe = insularObserverFactory(IntersectionObserver, {threshold: [0, 0.5]})
+type EntryCallback = (entry: IntersectionObserverEntry) => void
+const observeTypeCheck1: (target: Element, callback: EntryCallback) => void = observe
+const observeTypeCheck2: (target: Element, options: IntersectionObserverInit, callback: EntryCallback) => void = observe
+const unobserve = observe(document.body, (entry: IntersectionObserverEntry) => void 0)
+const unobserveTypeCheck: () => void = unobserve
