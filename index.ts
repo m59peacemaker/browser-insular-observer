cc647be5b77dc7c7e7e1e094fd3677caaf2a11e8
Martin Jurča <martin.jurca@firma.seznam.cz>
2019-08-07 11:16:19 +0200

    removed the "I" prefix from interface names

--- index.ts
+++ index.ts
@@ -1,32 +1,32 @@
 import keyMaster = require('key-master')
 
-interface IObserver<T, O> {
+interface Observer<T, O> {
   observe(target: T, options?: O): void
   unobserve?(target: T): void
 }
 
-interface IObserverEntry<T> {
+interface ObserverEntry<T> {
   target: T
 }
 
-type ObserverCallback<T, E extends IObserverEntry<T>> = (entries: E[]) => void
+type ObserverCallback<T, E extends ObserverEntry<T>> = (entries: E[]) => void
 
-type ObserverClass<T, O, E extends IObserverEntry<T>> = (
-  new(callback: ObserverCallback<T, E>, options?: O) => IObserver<T, O>
+type ObserverClass<T, O, E extends ObserverEntry<T>> = (
+  new(callback: ObserverCallback<T, E>, options?: O) => Observer<T, O>
 )
 
-type TargetListener<T, E extends IObserverEntry<T>> = (entry: E) => void
+type TargetListener<T, E extends ObserverEntry<T>> = (entry: E) => void
 
-interface IObserveCallback<T, O, E extends IObserverEntry<T>, L extends TargetListener<T, E>> {
+interface ObserveCallback<T, O, E extends ObserverEntry<T>, L extends TargetListener<T, E>> {
   (target: T, listener: L): UnobserveCallback
   (target: T, options: O, listener: L): UnobserveCallback
 }
 type UnobserveCallback = () => void
 
-const InsularObserver = <T extends object, O, E extends IObserverEntry<T>, L extends TargetListener<T, E>>(
-  Observer: ObserverClass<T, O, E>,
+const InsularObserver = <T extends object, O, E extends ObserverEntry<T>, L extends TargetListener<T, E>>(
+  ObserverConstructor: ObserverClass<T, O, E>,
   options?: O,
-): IObserveCallback<T, O, E, L> => {
+): ObserveCallback<T, O, E, L> => {
   const listeners = keyMaster<T, L[]>(() => [], new WeakMap<T, L[]>())
 
   const callback = (entries: E[]) => {
@@ -36,7 +36,7 @@ const InsularObserver = <T extends object, O, E extends IObserverEntry<T>, L ext
     })
   }
 
-  const observer = new Observer(callback, options)
+  const observer = new ObserverConstructor(callback, options)
 
   function observe (target: T, observeOptions: undefined | O | L, listener?: L): UnobserveCallback {
     if (typeof observeOptions === 'function') {
